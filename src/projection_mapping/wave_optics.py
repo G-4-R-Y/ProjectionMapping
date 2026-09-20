e83a271d58e7d251a80c4694f27760b017fb7645
Ricardo Corso Fernandes Junior <rcf.jr@hotmail.com>
2026-09-20 18:02:28 -0300

    fix: repair GLSL palette returns

--- src/projection_mapping/wave_optics.py
+++ src/projection_mapping/wave_optics.py
@@ -39,11 +39,11 @@ float sinc(float x){return abs(x)<1e-4?1.0:sin(x)/x;}
 vec3 pal(float t){
     if(u_palette==0) return vec3(.5+.5*cos(TAU*(t+.50)), .5+.5*cos(TAU*(t+.00)), .5+.5*cos(TAU*(t+.50)));
     t=fract(t);
-    if(u_palette==2)$1 .50+.50*cos(TAU*(vec3(1.0,.82,.63)*t+vec3(.56,.11,.02)));
-    if(u_palette==3)$1 .50+.50*cos(TAU*(vec3(1.0,.74,.55)*t+vec3(.02,.08,.16)));
-    if(u_palette==4)$1 .50+.50*cos(TAU*(vec3(.84,1.0,.70)*t+vec3(.42,.03,.20)));
-    if(u_palette==5)$1 .50+.50*cos(TAU*(vec3(.94,.74,1.0)*t+vec3(.72,.21,.03)));
-    if(u_palette==6)$1 .50+.50*cos(TAU*(vec3(1.0,.72,.82)*t+vec3(.55,.90,.12)));
+    if(u_palette==2) return .50+.50*cos(TAU*(vec3(1.0,.82,.63)*t+vec3(.56,.11,.02)));
+    if(u_palette==3) return .50+.50*cos(TAU*(vec3(1.0,.74,.55)*t+vec3(.02,.08,.16)));
+    if(u_palette==4) return .50+.50*cos(TAU*(vec3(.84,1.0,.70)*t+vec3(.42,.03,.20)));
+    if(u_palette==5) return .50+.50*cos(TAU*(vec3(.94,.74,1.0)*t+vec3(.72,.21,.03)));
+    if(u_palette==6) return .50+.50*cos(TAU*(vec3(1.0,.72,.82)*t+vec3(.55,.90,.12)));
     return .50+.50*cos(TAU*(vec3(1.0,.87,.72)*t+vec3(.01,.17,.44)));
 }
 
